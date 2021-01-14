dfd60ce95b3c8e6908ca8e807b0b38d98c612fd6
Cashman1396 <Chrisashman96@gmail.com>
2021-01-14 10:10:16 -0500

    added App code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const AppTitle = styled.h1`
+  display: block;
+  height: 64px; 
+  margin: 0;
+  padding: 20px 0;
+  font-size 30px;
+  text-transform: uppercase;
+  font-weight: 400;
+  color: #ffffff;
+  transition: 0.3s 1.4s;
+  opacity: ${({ showLabel }) => (showLabel ? 1 : 0)};
+
+  ${({ secondary }) =>
+  secondary &&
+  `
+  opacity: 1;
+  height: auto;
+  position: relative;
+  padding: 20px 0;
+  font-size: 30px;
+  top: 20%;
+  text-align: center;
+  transition: .5s;
+  @media ${device.tablet} {
+    font-size: 40px;
+  }
+  @media ${device.laptop} {
+    font-size: 50px;
+  }
+  @media ${device.laptopL} {
+    font-size: 60px;
+  }
+  @media ${device.desktop} {
+    font-size: 70px;
+  }
+  
+`}
+
+${({ showResult }) =>
+showResult &&
+`
+opacity: 0;
+visibility: hidden;
+top: 10%;
+`}
+
+
+`;
+
+const WeatherWrapper = styled.div`
+  max-width: 1500px;
+  margin: 0 auto;
+  height: calc(100vh - 64px);
+  width: 100%;
+  position: relative;
+`;
+
+  class App extends Component {
+    state = {
+      value: '',
+      weatherInfo: null,
+      error: false,
+    };
+    
+  }
+
+  
+
