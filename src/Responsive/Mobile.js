95fa6e1ca0d5539bfd75801f91195337822a16a2
Cashman1396 <Chrisashman96@gmail.com>
2021-01-17 09:36:27 -0500

    removed files

--- src/Responsive/Mobile.js
+++ src/Responsive/Mobile.js
@@ -7,3 +7,16 @@ const size = {
     laptopL: '1440px',
     desktop: '2560px',
   };
+
+  const mobile = {
+    mobileS: `(min-width: ${size.mobileS})`,
+    mobileM: `(min-width: ${size.mobileM})`,
+    mobileL: `(min-width: ${size.mobileL})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+    laptopL: `(min-width: ${size.laptopL})`,
+    desktop: `(min-width: ${size.desktop})`,
+    desktopL: `(min-width: ${size.desktop})`,
+  }
+
+  export default mobile
