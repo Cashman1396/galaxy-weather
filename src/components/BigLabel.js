492574259260a958047ee5cdc867f2c17de6576d
Cashman1396 <Chrisashman96@gmail.com>
2021-01-19 20:36:49 -0500

    added big label code

--- src/components/BigLabel.js
+++ src/components/BigLabel.js
@@ -1,1 +1,28 @@
 import styled from 'styled-components'
+import mobile from '../Responsive/Mobile'
+
+
+const BigLabel = styled.h2`
+color: ${({ color }) => color || '#FFFFFF'};
+  display: block;
+  font-weight: ${({ weight }) => weight || '600'};
+  font-size: ${({ fontSize }) => fontSize || '30px'};
+  text-align: ${({ align }) => align || 'left'};
+  padding: 5px 0;
+  ${({ firstToUpperCase }) =>
+    firstToUpperCase &&
+    ` &:first-letter {
+        text-transform: uppercase;
+      }
+      `}
+      @media ${device.tablet} {
+        font-size: ${({ fontSize }) => fontSize || '37px'};
+      }
+      @media ${device.laptop} {
+        font-size: ${({ fontSize }) => fontSize || '43px'};
+      } 
+      @media ${device.laptopL} {
+        font-size: ${({ fontSize }) => fontSize || '52px'};
+      } 
+    `;
+
