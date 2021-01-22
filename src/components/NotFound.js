b2bb4da17249d55e3a26568d1a128eb1bf21ba91
Cashman1396 <Chrisashman96@gmail.com>
2021-01-22 02:50:31 -0500

    added new file and not found city code

---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { FontAwesomeIcon } from  '@fortawesome/react-fontawesome';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
+import  styled from 'styled-components';
+import ResultFadeIn from './ResultFadeIn'
+
+
