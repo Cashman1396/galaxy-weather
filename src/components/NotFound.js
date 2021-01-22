474f396327da9a676c41f96a2e038134ab0b01cc
Cashman1396 <Chrisashman96@gmail.com>
2021-01-22 02:52:36 -0500

    added wrap code

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,4 +4,19 @@ import { faFrown } from '@fortawesome/free-solid-svg-icons';
 import  styled from 'styled-components';
 import ResultFadeIn from './ResultFadeIn'
 
-
+const NotFoundWrap = styled.div`
+  max-width: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 100px auto 0;
+  padding: 20px;
+  opacity: 0;
+  visibility: hidden;
+  position: relative;
+  border-radius: 10px;
+  top: 20px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
+  animation: ${ResultFadeIn} 0.5s 1.4s forwards;
+`;
