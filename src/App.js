3b4a5990dc6fd8ddbf5fe02ccb2cf1245b2b70fc
Cashman1396 <Chrisashman96@gmail.com>
2021-01-24 01:37:12 -0500

    finished months added days

--- src/App.js
+++ src/App.js
@@ -71,6 +71,47 @@ const WeatherWrapper = styled.div`
         value: e.target.value,
       })
     }
+
+    handleSearchCity = e => {
+      e.preventDefault();
+      const APIkey = process.env.REACT_APP_API_KEY
+      const weather = `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${APIkey}&units=metric`;
+      const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&APPID=${APIkey}&units=metric`;
+
+      Promise.all([fetch(weather), fetch(forecast)])
+        .then(([res1, res2]) => {
+          if (res1.ok && res2.ok) {
+            return Promise.all([res1.json(), res2.json()]);
+          }
+          throw Error(res1.statusText, res2.statusText)
+
+        })
+        .then(([data1, data2]) => {
+          const months =  [
+            'January',
+            'Febuary',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+          ]
+          const days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+          ]
+        })
+    }
     
   }
 
