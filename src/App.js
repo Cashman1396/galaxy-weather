89ea7acafece7a4ac389868cdd4ae1b3ec294569
Cashman1396 <Chrisashman96@gmail.com>
2021-01-25 12:55:19 -0500

    weather info object

--- src/App.js
+++ src/App.js
@@ -111,7 +111,27 @@ const WeatherWrapper = styled.div`
             'Saturday'
           ]
           const currentDate = new Date();
-          const date = `${day[currentDate.getDate()]} ${currentDate.getDate()} ${months[currentDate.getMonth()]}` 
+          const date = `${day[currentDate.getDate()]} ${currentDate.getDate()} ${months[currentDate.getMonth()]}`;
+          
+          const sunset = new Date(data1.sys.sunset * 1000).toLocaleTimeString().slice(0,5)
+          const sunrise = new Date(data1.sys.sunrise * 1000).toLocaleTimeString().slice(0,5)
+
+          const weatherInfo = {
+            city: data1.name,
+            country: data1.sys.country,
+            date,
+            description: data1.weather[0].description,
+            main: data1.weather[0].main,
+            temp: data1.main.temp,
+            highestTemp: data1.main.temp_max,
+            lowestTemp: data1.main.temp_min,
+            sunrise,
+            sunset,
+            clouds: data1.clouds.all,
+            humidity: data1.main.humidity,
+            wind: data1.wind.speed,
+            forecast: data2.list,
+          }
         })
     }
     
