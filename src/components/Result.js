3be9250b84c2815aa3ad2f26b02002f999460802
Cashman1396 <Chrisashman96@gmail.com>
2021-01-28 02:10:35 -0500

    added wraps

--- src/components/Result.js
+++ src/components/Result.js
@@ -12,3 +12,38 @@ import SmallLabel from './SmallLabel';
 import MediumLabel from './MediumLabel';
 import BigLabel from './BigLabel';
 import Text from './Text';
+
+const Results = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 40px 0;
+  opacity: 0;
+  visibility: hidden;
+  position: relative;
+  top: 20px;
+  animation: ${ResultFadeIn} 0.5s 1.4s forwards;
+`;
+
+const LocationWrap = styled.div`
+  flex-basis: 100%;
+`;
+
+
+const CurrentWeatherWrap = styled.div`
+  flex-basis: 100%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: auto 1fr;
+  margin: 20px 0;
+  grid-gap: 30px;
+  @media ${device.mobileL} {
+    flex-basis: 50%;
+    padding-right: 10px;
+  }
+  @media ${device.tablet} {
+    grid-template-columns: 1fr 1fr;
+    padding-right: 20px;
+  }
+`;
