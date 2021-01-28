b3f3faf33501567a128bcd48e23caa77a5e90ab4
Cashman1396 <Chrisashman96@gmail.com>
2021-01-28 02:11:10 -0500

    added weather icon

--- src/components/Result.js
+++ src/components/Result.js
@@ -47,3 +47,21 @@ const CurrentWeatherWrap = styled.div`
     padding-right: 20px;
   }
 `;
+
+const WeatherIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 70px;
+  color: #ffffff;
+  @media ${device.tablet} {
+    font-size: 100px;
+    justify-content: flex-end;
+  }
+  @media ${device.laptop} {
+    font-size: 120px;
+  }
+  @media ${device.laptopL} {
+    font-size: 140px;
+  }
+`;
