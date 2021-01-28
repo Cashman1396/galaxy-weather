2dd71cb7028193d2f03f566a5d7900ea1fba40bf
Cashman1396 <Chrisashman96@gmail.com>
2021-01-28 01:46:12 -0500

    added code for forecast

---
+++ src/components/ForecastHour.js
@@ -0,0 +1,6 @@
+import React from ' react';
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import SmallLabel from './SmallLabel'
+import Text from './Text'
+import mobile from '../Responsive/Mobile'
