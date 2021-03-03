bf3ffc9b5fe1288cdb1b086ca94ad6a80b31f944
Cashman1396 <Chrisashman96@gmail.com>
2021-03-03 17:00:20 -0500

    worked results

--- src/components/Result.js
+++ src/components/Result.js
@@ -152,7 +152,105 @@ const Result = ({ weather }) => {
     day={item.dt_txt.slice(8, 10)}
     hour={item.dt_txt.slice(11, 13) * 1}
     />
-  ))
+  ));
+
+  let WeatherIcon = null;
+
+  if (main === 'Thunderstorm') {
+    weatherIcon = <FontAwesomeIcon icon={faBolt} />;
+  } else if (main === 'Drizzle') {
+    weatherIcon = <FontAwesomeIcon icon={faCloudRain} />;
+  } else if (main === 'Rain') {
+    weatherIcon = <FontAwesomeIcon icon={faCloudShowersHeavy} />;
+  } else if (main === 'Snow') {
+    weatherIcon = <FontAwesomeIcon icon={faSnowflake} />;
+  } else if (main === 'Clear') {
+    weatherIcon = <FontAwesomeIcon icon={faSun} />;
+  } else if (main === 'Clouds') {
+    weatherIcon = <FontAwesomeIcon icon={faCloud} />;
+  } else {
+    weatherIcon = <FontAwesomeIcon icon={faSmog} />;
+  }
+
+  eturn (
+    <Results>
+      <LocationWrap>
+        <BigLabel>
+          {city}, {country}
+        </BigLabel>
+        <SmallLabel weight="400">{date}</SmallLabel>
+      </LocationWrap>
+      <CurrentWeatherWrap>
+        <WeatherIcon>{weatherIcon}</WeatherIcon>
+        <TemperatureWrap>
+          <Temperature>{Math.floor(temp)}&#176;</Temperature>
+          <SmallLabel weight="400" firstToUpperCase>
+            {description}
+          </SmallLabel>
+        </TemperatureWrap>
+      </CurrentWeatherWrap>
+      <WeatherDetailsWrap>
+        <WeatherDetail>
+          <SmallLabel align="center" weight="400">
+            {Math.floor(highestTemp)}&#176;
+          </SmallLabel>
+          <Text align="center">Hight</Text>
+        </WeatherDetail>
+        <WeatherDetail>
+          <SmallLabel align="center" weight="400">
+            {wind}mph
+          </SmallLabel>
+          <Text align="center">Wind</Text>
+        </WeatherDetail>
+        <WeatherDetail>
+          <SmallLabel align="center" weight="400">
+            {sunrise}
+          </SmallLabel>
+          <Text align="center">Sunrise</Text>
+        </WeatherDetail>
+        <WeatherDetail>
+          <SmallLabel align="center" weight="400">
+            {Math.floor(lowestTemp)}&#176;
+          </SmallLabel>
+          <Text align="center">Low</Text>
+        </WeatherDetail>
+        <WeatherDetail>
+          <SmallLabel align="center" weight="400">
+            {humidity}%
+          </SmallLabel>
+          <Text align="center">Rain</Text>
+        </WeatherDetail>
+        <WeatherDetail>
+          <SmallLabel align="center" weight="400">
+            {sunset}
+          </SmallLabel>
+          <Text align="center">Sunset</Text>
+        </WeatherDetail>
+      </WeatherDetailsWrap>
+      <ForecastWrap>
+        <MediumLabel weight="400">Forecast</MediumLabel>
+        <Forecast>{forecasts}</Forecast>
+      </ForecastWrap>
+    </Results>
+  );
+};
+
+Result.propTypes = {
+  weather: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+    date: PropTypes.string,
+    description: PropTypes.string,
+    main: PropTypes.string,
+    temp: PropTypes.number,
+    sunrise: PropTypes.string,
+    sunset: PropTypes.string,
+    humidity: PropTypes.number,
+    wind: PropTypes.number,
+    highestTemp: PropTypes.number,
+    lowestTemp: PropTypes.number,
+    forecast: PropTypes.array,
+  }).isRequired,
 }
 
 export default Result
